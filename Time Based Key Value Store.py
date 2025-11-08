a870220ac7a8452e9739fa4a0a7ff6f90f621402
Tobi Ashiru <tobiashiru58@gmail.com>
2025-11-08 15:49:15 +0000

    Update Time Based Key Value Store.py

--- Time Based Key Value Store.py
+++ Time Based Key Value Store.py
@@ -1,13 +1,13 @@
 class TimeMap:
 
     def __init__(self):
-        self.store = {"foo":[(1,"bar"),(4,"bar2")]}
+        self.store = {}
 
     def set(self, key: str, value: str, timestamp: int) -> None:
         if key not in self.store:
-            self.store[key] = [(timestamp, value)]
+            self.store[key] = [(timestamp,value)]
         else:
-            self.store[key].append((timestamp, value))
+            self.store[key].append((timestamp,value))
         
 
     def get(self, key: str, timestamp: int) -> str:
@@ -17,18 +17,14 @@ class TimeMap:
             return ""
         left = 0
         right = len(self.store[key]) - 1
-        result = -1
-
         while left <= right:
-            mid = (left+right) // 2
+            mid = (left+right)//2
             if self.store[key][mid][0] <= timestamp:
-                result = mid
                 left = mid + 1
             else:
                 right = mid - 1
-        if result == -1:
-            return ""
         return self.store[key][right][1]
+            
 
 
 # Your TimeMap object will be instantiated and called as such:
